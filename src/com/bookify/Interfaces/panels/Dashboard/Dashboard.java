2c128123fc57bd0ee44be2e8aae1aefd494bb8e8
Chamod Dilshan <chamoddilshan834@gmail.com>
2025-08-21 15:11:31 +0530

    Bug fix

--- src/com/bookify/Interfaces/panels/Dashboard/Dashboard.java
+++ src/com/bookify/Interfaces/panels/Dashboard/Dashboard.java
@@ -2,11 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
  */
-package com.bookify.Interfaces.panels.Dashboard;
+package com.bookify.Interfaces.Panels.Dashboard;
 
 /**
  *
- * @author Ayomal Kaushalya
+ * @author chamo
  */
 public class Dashboard extends javax.swing.JPanel {
 
