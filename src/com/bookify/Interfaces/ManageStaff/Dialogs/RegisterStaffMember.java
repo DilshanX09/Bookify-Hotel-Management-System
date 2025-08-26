6bd22492ecc2437d6ffb85198dd99c95bfbc0134
Chamod Dilshan <chamoddilshan834@gmail.com>
2025-08-26 18:34:38 +0530

    Implement RegisterStaffMember Frame with Singleton pattern and CardLayout (personal details panel + employment details panel navigation)

--- src/com/bookify/Interfaces/ManageStaff/Dialogs/RegisterStaffMember.java
+++ src/com/bookify/Interfaces/ManageStaff/Dialogs/RegisterStaffMember.java
@@ -1,268 +1,86 @@
-
 package com.bookify.Interfaces.ManageStaff.Dialogs;
 
-import com.bookify.Interfaces.ManageStaff.Dialogs.NextRegisterStaffMember;
-import com.bookify.Interfaces.ManageStaff.Panels.ManageStaff;
-import com.formdev.flatlaf.FlatClientProperties;
-import com.formdev.flatlaf.extras.FlatSVGIcon;
+import com.bookify.Interfaces.ManageStaff.Dialogs.Panels.EmploymentDetailsPanel;
+import com.bookify.Interfaces.ManageStaff.Dialogs.Panels.PersonalDetailsPanel;
+import java.awt.CardLayout;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
-public class RegisterStaffMember extends javax.swing.JDialog {
+public class RegisterStaffMember extends javax.swing.JFrame {
 
-    ManageStaff m;
+    private PersonalDetailsPanel personalDetailsPanel;
+    private EmploymentDetailsPanel employmentDetailsPanel;
+    private CardLayout cardLayout;
+    private static RegisterStaffMember dialog;
 
-    public RegisterStaffMember(ManageStaff parent, java.awt.Frame frameParent, boolean modal) {
-        super(frameParent, modal);
+    public RegisterStaffMember() {
         initComponents();
-        init();
-        this.m = parent;
+        loadPanels();
     }
 
-    private void init() {
-        homeIcon.setIcon(new FlatSVGIcon("com/bookify/Resources/Icons/Home.svg", homeIcon.getWidth(), homeIcon.getHeight()));
-        nextBtn.putClientProperty(FlatClientProperties.STYLE, "arc:12");
-        firstNameInput.putClientProperty(FlatClientProperties.STYLE, "arc:12");
-        contactNumberInput.putClientProperty(FlatClientProperties.STYLE, "arc:12");
-        emailInput.putClientProperty(FlatClientProperties.STYLE, "arc:12");
-        gender.putClientProperty(FlatClientProperties.STYLE, "arc:12");
-        birthDateChooser.putClientProperty(FlatClientProperties.STYLE, "arc:12");
+    public static RegisterStaffMember getInstance() {
+        if (dialog == null) {
+            dialog = new RegisterStaffMember();
+        }
+        return dialog;
+    }
+
+    public JPanel getMainLayout() {
+        return mainLayout;
+    }
+
+    public CardLayout getCardLayout() {
+        return cardLayout;
+    }
+
+    private void loadPanels() {
+        if (cardLayout == null && mainLayout.getLayout() instanceof CardLayout) {
+            this.cardLayout = (CardLayout) mainLayout.getLayout();
+        }
+
+        this.personalDetailsPanel = new PersonalDetailsPanel();
+        this.employmentDetailsPanel = new EmploymentDetailsPanel();
+
+        this.mainLayout.add(personalDetailsPanel, "personalDetailsPanel");
+        this.mainLayout.add(employmentDetailsPanel, "employmentDetailsPanel");
+
+        this.cardLayout.show(mainLayout, "personalDetailsPanel");
+
+        SwingUtilities.updateComponentTreeUI(mainLayout);
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
-        homeIcon = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        nextBtn = new javax.swing.JButton();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-        firstNameInput = new javax.swing.JTextField();
-        lastNameInput = new javax.swing.JTextField();
-        jLabel8 = new javax.swing.JLabel();
-        jLabel9 = new javax.swing.JLabel();
-        contactNumberInput = new javax.swing.JTextField();
-        emailInput = new javax.swing.JTextField();
-        jLabel10 = new javax.swing.JLabel();
-        jLabel11 = new javax.swing.JLabel();
-        gender = new javax.swing.JTextField();
-        birthDateChooser = new com.toedter.calendar.JDateChooser();
+        mainLayout = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setMaximumSize(new java.awt.Dimension(449, 573));
+        setMinimumSize(new java.awt.Dimension(449, 573));
+        setPreferredSize(null);
+        setResizable(false);
 
-        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
-
-        homeIcon.setBackground(new java.awt.Color(255, 255, 255));
-        homeIcon.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
-        homeIcon.setOpaque(true);
-
-        jLabel2.setBackground(new java.awt.Color(100, 100, 100));
-        jLabel2.setFont(new java.awt.Font("Inter 18pt Medium", 0, 13)); // NOI18N
-        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel2.setText("/  Staff Management");
-
-        jLabel3.setBackground(new java.awt.Color(100, 100, 100));
-        jLabel3.setFont(new java.awt.Font("Inter 18pt Medium", 0, 13)); // NOI18N
-        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel3.setText("/  Register Staff Member");
-
-        nextBtn.setBackground(new java.awt.Color(0, 0, 0));
-        nextBtn.setFont(new java.awt.Font("Inter 18pt Medium", 0, 13)); // NOI18N
-        nextBtn.setForeground(new java.awt.Color(255, 255, 255));
-        nextBtn.setText("Next");
-        nextBtn.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        nextBtn.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                nextBtnActionPerformed(evt);
-            }
-        });
-
-        jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
-        jLabel4.setText("Register Staff Member");
-
-        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        jLabel1.setText("Personal Details");
-
-        jLabel5.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jLabel5.setText("Enter the staff member's details, assign a role, and set system access if required.");
-
-        jLabel6.setText("First Name");
-
-        jLabel7.setText("Last Name");
-
-        jLabel8.setText("Contact Number");
-
-        jLabel9.setText("Email Address");
-
-        jLabel10.setText("Gender");
-
-        jLabel11.setText("Date of Birth");
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(48, 48, 48)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(0, 0, Short.MAX_VALUE)
-                                .addComponent(nextBtn))
-                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                                .addComponent(jLabel6)
-                                                .addComponent(jLabel8)
-                                                .addComponent(jLabel10))
-                                            .addGap(178, 178, 178))
-                                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                                .addComponent(gender, javax.swing.GroupLayout.Alignment.LEADING)
-                                                .addComponent(contactNumberInput))
-                                            .addGap(33, 33, 33)))
-                                    .addGroup(jPanel1Layout.createSequentialGroup()
-                                        .addComponent(firstNameInput, javax.swing.GroupLayout.PREFERRED_SIZE, 234, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addGap(31, 31, 31)))
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(birthDateChooser, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(emailInput)
-                                    .addGroup(jPanel1Layout.createSequentialGroup()
-                                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                            .addComponent(jLabel11)
-                                            .addComponent(jLabel9)
-                                            .addComponent(jLabel7))
-                                        .addGap(0, 0, Short.MAX_VALUE))
-                                    .addComponent(lastNameInput))))
-                        .addGap(36, 36, 36))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
-                            .addComponent(jLabel4)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(homeIcon, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jLabel2)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jLabel3))
-                            .addComponent(jLabel5))
-                        .addContainerGap(41, Short.MAX_VALUE))))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(33, 33, 33)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
-                    .addComponent(jLabel2)
-                    .addComponent(homeIcon, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel3))
-                .addGap(18, 18, 18)
-                .addComponent(jLabel4)
-                .addGap(18, 18, 18)
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel5)
-                .addGap(43, 43, 43)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel6)
-                    .addComponent(jLabel7))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(firstNameInput, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(lastNameInput, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel8)
-                    .addComponent(jLabel9))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(contactNumberInput, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(emailInput, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel10)
-                    .addComponent(jLabel11))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(birthDateChooser, javax.swing.GroupLayout.DEFAULT_SIZE, 34, Short.MAX_VALUE)
-                    .addComponent(gender))
-                .addGap(38, 38, 38)
-                .addComponent(nextBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(30, 30, 30))
-        );
+        mainLayout.setBackground(new java.awt.Color(255, 255, 255));
+        mainLayout.setMinimumSize(new java.awt.Dimension(449, 587));
+        mainLayout.setLayout(new java.awt.CardLayout());
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(mainLayout, javax.swing.GroupLayout.DEFAULT_SIZE, 449, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(mainLayout, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void nextBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nextBtnActionPerformed
-      
-    
-    }//GEN-LAST:event_nextBtnActionPerformed
-
-    public static void main(String args[]) {
-    /* Set the Nimbus look and feel */
-    //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-    /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-     */
-    try {
-        for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-            if ("Nimbus".equals(info.getName())) {
-                javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                break;
-            }
-        }
-    } catch (ClassNotFoundException ex) {
-        java.util.logging.Logger.getLogger(RegisterStaffMember.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-    } catch (InstantiationException ex) {
-        java.util.logging.Logger.getLogger(RegisterStaffMember.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-    } catch (IllegalAccessException ex) {
-        java.util.logging.Logger.getLogger(RegisterStaffMember.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-    } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-        java.util.logging.Logger.getLogger(RegisterStaffMember.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-    }
-    //</editor-fold>
-
-}
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private com.toedter.calendar.JDateChooser birthDateChooser;
-    private javax.swing.JTextField contactNumberInput;
-    private javax.swing.JTextField emailInput;
-    private javax.swing.JTextField firstNameInput;
-    private javax.swing.JTextField gender;
-    private javax.swing.JLabel homeIcon;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel10;
-    private javax.swing.JLabel jLabel11;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
-    private javax.swing.JLabel jLabel9;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JTextField lastNameInput;
-    private javax.swing.JButton nextBtn;
+    private javax.swing.JPanel mainLayout;
     // End of variables declaration//GEN-END:variables
 }
